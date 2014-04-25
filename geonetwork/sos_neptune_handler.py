feece09965229ed42fcc5893a514007e99768d9b
oceanzus <case@oceanz.org>
2014-04-25 09:08:55 -0700

    updated the neptune handler to work on produciton like env
    add coverage model service implementation stub
    
    update port connection to use yml file
    
    update yml file
    
    Update README.md

--- geonetwork/sos_neptune_handler.py
+++ geonetwork/sos_neptune_handler.py
@@ -16,10 +16,25 @@ __author__ = "abird"
 
 class Handler():
 	def __init__(self):
-		self.PORT = 5454
+		logger = logging.getLogger('importer_service')
+        hdlr = logging.FileHandler('importer_service.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr) 
+        logger.setLevel(logging.DEBUG)
+
+        self.logger = logger
+        self.logger.info("Setting up neptune handler service...")
+
 		self.startup()
 
 	def startup(self):	
+
+		stream = open("../extern.yml", 'r')
+        ion_config = yaml.load(stream)
+
+        self.PORT = ion_config['eoi']['neptune_sos_handler']['port']
+        self.logger.info('Serving Neptune Handler on '+str(self.PORT)+'...')
 		server = WSGIServer(('', self.PORT), self.application).serve_forever()
 
 	def application(self,env, start_response):	        
@@ -35,23 +50,23 @@ class Handler():
 	     		print "query:" + env['QUERY_STRING']		       
 		        request = request[1:]
 		        print "request:"+request
-
-		        output = ''		        
+                        request = env['QUERY_STRING']
+     		        output = ''		        
 		        #print "env:"+str(env)
 		        neptune_sos_link = "http://dmas.uvic.ca/sos?"+request		        
 		        r_text = requests.get(neptune_sos_link)
-		        print "---end of request---"
+			print "---end of request---"
 
 		        #fix the crs code
-		        soup = BeautifulSoup(r_text.text)
-		        offering_list = soup.findAll("sos:observationoffering")
-		        for offering in offering_list:
-		        	envelopes = soup.findAll("gml:envelope")    
-		        	for e in envelopes:
-		        		e['srsName'] = "urn:ogc:def:crs:EPSG:6.5:4326"
+		        soup = BeautifulSoup(r_text.text,"xml")
+			for env in soup.findAll("Envelope"):
+				env['srsName'] = "urn:ogc:def:crs:EPSG:6.5:4326" 
 		        
-		      
-		        response_headers = [('Content-Type', 'text/xml; charset=utf-8')]
+			text_file = open("Output.html", "w")
+			text_file.write("%s" % str(soup))
+			text_file.close()
+		        
+			response_headers = [('Content-Type', 'application/xml; charset=utf-8')]
 		        status = '200 OK'
 		        #remove the html codes i
 		        html_start = "<html><body>"
@@ -61,10 +76,11 @@ class Handler():
 		        	xm_response = xm_response.replace(html_start, "");
 		        if xm_response.endswith(html_end):	
 		        	xm_response = xm_response.replace(html_end,"");
-				
-				#add the xml heeader
-				xm_response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+xm_response
-				
+		        #xm_response.replace("http://schemas.opengis.net/sos/1.0.0/sosAll.xsd","http://schemas.opengis.net/sos/1.0.0/sosGetCapabilities.xsd")		
+			#add the xml heeader
+			#xm_response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+xm_response
+			print "----------------"
+			#print "xmlresp:\n"+xm_response[:]
 	        	start_response(status, response_headers)
         		return [xm_response]
 	        

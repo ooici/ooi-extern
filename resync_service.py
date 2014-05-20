6e15ea4825af31d3e1a3cea73f3458d6661a9805
Jim Case <case@oceanz.org>
2014-05-20 13:23:55 -0400

    Adds metadata synchronization handling
    Adds metadata synchronization handling between GeoNetwork and the OOI Resource Registry using the SGS and a new table in the GeoNetwork PostgreSQL database called metadataregistry.

--- resync_service.py
+++ resync_service.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-WSGI geonetwork metadata resync service
+WSGI GeoNetwork to OOI Resource Registry Metadata Synchronization Service
 """
 from gevent.pywsgi import WSGIServer
 import httplib2
@@ -17,208 +17,256 @@ import numpy as np
 
 __author__ = "abird"
 
-#see the folling for reference
-#http://geonetwork-opensource.org/manuals/trunk/eng/developer/xml_services/metadata_xml_search_retrieve.html#search-metadata-xml-search
+# USAGE: See the following for reference:
+# http://geonetwork-opensource.org/manuals/trunk/eng/developer/xml_services/metadata_xml_search_retrieve.html#search-metadata-xml-search
 
-#headers
+# Content headers
 headers = {'content-type': 'application/xml'}
+
 sync_harvesters = "syncharvesters"
 
 
 class DataProductImporter():
-	def __init__(self):
-		logger = logging.getLogger('resync_service')
-		hdlr = logging.FileHandler('resync_service.log')
-		formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-		hdlr.setFormatter(formatter)
-		logger.addHandler(hdlr)
-		logger.setLevel(logging.DEBUG)
+    def __init__(self):
+        logger = logging.getLogger('resync_service')
+        hdlr = logging.FileHandler('resync_service.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+        logger.setLevel(logging.DEBUG)
 
-		self.logger = logger
-		self.logger.info("Setting up geonetwork to RR resync service...")
-		self.startup()
-	def startup(self):	
-		stream = open("extern.yml", 'r')
-		ion_config = yaml.load(stream)
-		self.logger.info("opened yml file")
+        self.logger = logger
+        self.logger.info("Setting up geonetwork to RR resync service...")
+        self.startup()
 
-		##TODO add to yml file
-		self.GEONETWORK_SERVER = "http://r3-pg-test02.oceanobservatories.org:8080"
-		self.GEONETWORK_DB_SERVER = "r3-pg-test02.oceanobservatories.org"
-		self.GEONETWORK_DB = "geonetwork"
-		self.RR_PORT = 8848
-		self.GEONETWORK_USER = 'ooici'
-		self.GEONETWORK_PASS= 'ooici'
+    def startup(self):
+        stream = open("extern.yml", 'r')
+        ion_config = yaml.load(stream)
+        self.logger.info("opened yml file")
 
-		self.SGS_URL = self.url = 'http://%s:%s/ion-service/' % ('localhost', 5000)
+        ##TODO add to yml file
+        self.GEONETWORK_SERVER = "http://r3-pg-test02.oceanobservatories.org:8080"
+        self.GEONETWORK_DB_SERVER = "r3-pg-test02.oceanobservatories.org"
+        self.GEONETWORK_DB = "geonetwork"
+        self.RR_PORT = 8848
+        self.GEONETWORK_USER = 'ooici'
+        self.GEONETWORK_PASS= 'ooici'
 
+        self.SGS_URL = self.url = 'http://%s:%s/ion-service/' % ('localhost', 5000)
 
-		self.logger.info('Serving on '+str(self.RR_PORT)+'...')
-		server = WSGIServer(('', self.RR_PORT), self.application).serve_forever()
+        self.logger.info('Serving on '+str(self.RR_PORT)+'...')
+        server = WSGIServer(('', self.RR_PORT), self.application).serve_forever()
 
-	def application(self, env, start_response):	
-		request = env['PATH_INFO']
-		request = request[1:]
-		output = ''
-		if request == '/':
-			start_response('404 Not Found', [('Content-Type', 'text/html')])
-			return ["<h1>Error<b>please add request information</b>"]
-		else:
-			req = request.split("&")
-			param_dict = {}
-			if len(req) > 1:
-				for param in req:
-					params = param.split("=")
- 					param_dict[params[0]] = params[1]
+    def application(self, env, start_response):
+        request = env['PATH_INFO']
+        request = request[1:]
+        output = ''
+        if request == '/':
+            start_response('404 Not Found', [('Content-Type', 'text/html')])
+            return ["<h1>Error<b>please add request information</b>"]
+        else:
+            req = request.split("&")
+            param_dict = {}
+            if len(req) > 1:
+                for param in req:
+                    params = param.split("=")
+                    param_dict[params[0]] = params[1]
 
-				if param_dict.has_key(sync_harvesters) and param_dict.has_key("ooi"):
-					try:
-						print "requested sync harvesters:",param_dict[sync_harvesters]
-						site_dict = self.get_harvester_list()
-						#self.get_all_available_meta_data_records(site_dict)		
-						self.get_meta_data_records_for_harvester(site_dict)
+                if param_dict.has_key(sync_harvesters) and param_dict.has_key("ooi"):
+                    try:
+                        print "requested sync harvesters:",param_dict[sync_harvesters]
+                        site_dict = self.get_harvester_list()
+                        #self.get_all_available_meta_data_records(site_dict)
+                        self.get_meta_data_records_for_harvester(site_dict)
 
-						start_response('200 ok', [('Content-Type', 'text/html')])
-						return ['<b>ALIVE<BR>' + request + '</b>']
-					except Exception, e:
-						start_response('400 Bad Request', [('Content-Type', 'text/html')])
-						return ['<b>ERROR IN HARVESTER, CHECK CONNECTION<BR>' + request + '<br>' + output + '</b>']				
-					
-					
-				else:
-					start_response('400 Bad Request', [('Content-Type', 'text/html')])
-					return ['<b>ERROR IN PARAMS<BR>' + request + '<br>' + output + '</b>']					
-			else:
-				start_response('400 Bad Request', [('Content-Type', 'text/html')])
-				return ['<b>ERROR NO PARAMS<BR>' + request + '<br>' + output + '</b>']
-					
+                        start_response('200 ok', [('Content-Type', 'text/html')])
+                        return ['<b>ALIVE<BR>' + request + '</b>']
+                    except Exception, e:
+                        start_response('400 Bad Request', [('Content-Type', 'text/html')])
+                        return ['<b>ERROR IN HARVESTER, CHECK CONNECTION<BR>' + request + '<br>' + output + '</b>']
+                else:
+                    start_response('400 Bad Request', [('Content-Type', 'text/html')])
+                    return ['<b>ERROR IN PARAMS<BR>' + request + '<br>' + output + '</b>']
+            else:
+                start_response('400 Bad Request', [('Content-Type', 'text/html')])
+                return ['<b>ERROR NO PARAMS<BR>' + request + '<br>' + output + '</b>']
 
-	def get_harvester_list(self):
-		'''
-		creates a dict of the harvester names and id's that are valid 
-		'''
-		r = requests.get(self.GEONETWORK_SERVER+'/geonetwork/srv/eng/harvesting/xml.harvesting.get',auth=("admin","admin"),headers=headers)
-		r.status_code
-		soup = BeautifulSoup(r.text)
-		sites = soup.find_all("site")
-		site_dict = dict()
+    def get_harvester_list(self):
+        """
+        Creates a dict of valid harvester names and id's using GeoNetwork's XML REST service
+        """
+        r = requests.get(self.GEONETWORK_SERVER+'/geonetwork/srv/eng/harvesting/xml.harvesting.get', auth=("admin", "admin"), headers=headers)
+        r.status_code
+        soup = BeautifulSoup(r.text)
+        sites = soup.find_all("site")
+        site_dict = dict()
 
-		accept_list = ["neptune","ioos","ioos2"]
+        accept_list = ["neptune", "ioos", "ioos2"]
 
-		for site in sites:    
-			name = site.find("name").text
-			if name in accept_list:
-				uuid = site.find("uuid").text
-				site_dict[uuid] = name
-		return site_dict
+        for site in sites:
+            name = site.find("name").text
+            if name in accept_list:
+                uuid = site.find("uuid").text
+                site_dict[uuid] = name
+        return site_dict
 
-	def get_meta_data_records_for_harvester(self,site_dict):
-		records =[]
-		try:
-			conn = psycopg2.connect(database=self.GEONETWORK_DB, user=self.GEONETWORK_USER, password=self.GEONETWORK_PASS,host=self.GEONETWORK_DB_SERVER)
-			cursor = conn.cursor()
-			# execute our Query
-			for site_uuid in site_dict.keys():
-				cursor.execute("SELECT uuid,data from metadata WHERE harvestuuid='"+site_uuid+"' ")
-				records = cursor.fetchall()
-				print "number of records...",len(records)
-				for rec in records:
-					uuid = rec[0]
-					soup = BeautifulSoup(rec[1])
-					#get the identification information for the place
-					rec_name = uuid
-					rec_descrip = ""
-					try:						
-						rec_name = self.getnameinfo(soup)
-						rec_descrip = self.getidentinfo(soup)            
-						self.getKeyWords(soup)
-						self.getGeoExtent(soup)
-						dt = self.getTemporalExtent(soup)
-						print rec_name,rec_descrip
-					except Exception, e:
-						print "error getting nodes:",e,"\nUSING:",rec_name,"\n",rec_descrip
+    def get_meta_data_records_for_harvester(self, site_dict):
+        """
+        Lookup and extract metadata records from GeoNetwork and add to the RR
+        """
+        records = []
+        try:
+            conn = psycopg2.connect(database=self.GEONETWORK_DB, user=self.GEONETWORK_USER, password=self.GEONETWORK_PASS, host=self.GEONETWORK_DB_SERVER)
+            cursor = conn.cursor()
+            # execute our Query
+            for site_uuid in site_dict.keys():
+                cursor.execute("SELECT m.uuid,m.data,m.changedate,mr.registerdate,mr.rruuid,m.changedate NOT LIKE mr.registerdate AS mchanged FROM metadata m FULL JOIN metadataregistry mr ON m.uuid=mr.gnuuid WHERE m.harvestuuid='"+site_uuid+"' ")
+                records = cursor.fetchall()
+                print "number of records...", len(records)
+                for rec in records:
+                    uuid = rec[0]
 
-					#add the data to the RR	
-					self.request_and_create_resource('resource_registry', 'create', object ={"name":rec_name, "description":rec_descrip , "type_":"ExternalDataset"})	
+                    # Get the metadata record
+                    soup = BeautifulSoup(self.get_metadata_payload(uuid))
+                    #get the identification information for the place
+                    rec_name = uuid
+                    rec_descrip = ""
+                    try:
+                        rec_name = self.getnameinfo(soup)
+                        rec_descrip = self.getidentinfo(soup)
+                        #self.getkeywords(soup)
+                        #self.getgeoextent(soup)
+                        #dt = self.get_temporal_extent(soup)
+                        #print rec_name, rec_descrip
+                    except Exception, e:
+                        #print "error getting nodes:", e, "\nUSING:", rec_name, "\n", rec_descrip
+                        raise ValueError('Error getting name and description nodes from metadata record.')
 
-		except Exception, e:
-			print e,": I am unable to connect to the database..."
-			pass
+                    # Create RR entries and add/modify/delete lookups in metadataregistry table
+                    rec_changedate = rec[2]
+                    #rec_registerdate = rec[3]
+                    rec_rruuid = rec[4]
+                    rec_mchanged = rec[5]
+                    #add the data to the RR
+                    if rec_rruuid == None:
+                        # The metadata record is new
+                        gwresponse = self.request_resource_action('resource_registry', 'create', object={"name": rec_name, "description": rec_descrip, "type_": "ExternalDataset"})
+                        rruuid = gwresponse[0]
 
-	def request_and_create_resource(self,service_name, op, **kwargs):
-		url = self.SGS_URL
-		url = url + service_name + '/' + op
-		r = { "serviceRequest": { 
-				"serviceName" : service_name, 
-				"serviceOp" : op, 
-				"params" : kwargs
-				}
-			}
+                        # Add record to metadataregistry table with registerdate and rruuid
+                        insert_values = {'uuid': uuid, 'rruuid': rruuid, 'changedate': rec_changedate}
+                        insert_stmt = "INSERT INTO metadataregistry (gnuuid,rruuid,registerdate) VALUES ('%(uuid)s','%(rruuid)s','%(changedate)s')" % insert_values
+                        cursor.execute(insert_stmt)
+                    elif rec_mchanged:
+                        pass
+                        # The metadata record has changed
+                        # Delete existing Resource from the RR
+                        self.request_resource_action('resource_registry', 'delete', object_id=rec_rruuid)
 
+                        # Create new resource in the RR
+                        gwresponse = rruuid = self.request_resource_action('resource_registry', 'create', object={"name": rec_name, "description": rec_descrip, "type_": "ExternalDataset"})
+                        rruuid = gwresponse[0]
 
-		resp = requests.post(url, data={'payload':Serializer.encode(r)})
-		if resp.status_code == 200:
-			data = resp.json()
-			if 'GatewayError' in data['data']:
-				#error = GatewayError(data['data']['Message'])
-				#error.trace = data['data']['Trace']
-				#raise error
-				pass
-			if 'GatewayResponse' in data['data']:
-				#return data['data']['GatewayResponse']
-				pass
+                        # UPDATE metadataregistry table record with updated registerdate and rruuid
+                        update_values = {'uuid': uuid, 'rruuid': rruuid, 'changedate': rec_changedate}
+                        update_stmt = "UPDATE metadataregistry SET rruuid='%(rruuid)s', registerdate='%(changedate)s' WHERE gnuuid='%(uuid)s'" % update_values
+                        cursor.execute(update_stmt)
+                    elif uuid == None:
+                        # Metadata record was deleted by the harvester, cleanup the lookup table and the OOI RR
+                        # Delete from RR
+                        self.request_resource_action('resource_registry', 'delete', object_id=rec_rruuid)
 
-		#raise ConnectionError("HTTP [%s]" % resp.status_code)		
-	def getnameinfo(self,soup):
-		ab_info = soup.find("gmd:abstract")
-		pur_info = soup.find("gmd:purpose")
-		file_ident = soup.find("gmd:fileidentifier")
-		return file_ident.text.replace("\n","")
+                        # Delete from metadataregistry
+                        delete_values = ('rruuid': rec_rruuid)
+                        delete_stmt = "DELETE FROM metadataregistry WHERE rruuid='%(rruuid)s'" % delete_values
+                        cursor.execute(delete_stmt)
+        except Exception, e:
+            print e, ": I am unable to connect to the database..."
 
-	def getidentinfo(self,soup):
-		indent_info = soup.find("gmd:identificationinfo")
-		title = indent_info.find("gmd:title").text.rstrip()
-		alt_title = indent_info.find("gmd:alternatetitle").text.replace("\n","")
-		iden = indent_info.find("gmd:identifier").text.replace("\n","")
-		org_name = indent_info.find("gmd:organisationname").text.replace("\n","")
-		poc = indent_info.find("gmd:pointofcontact")
-		return title
+    def get_metadata_payload(self, uuid):
+        conn = psycopg2.connect(database=self.GEONETWORK_DB, user=self.GEONETWORK_USER, password=self.GEONETWORK_PASS, host=self.GEONETWORK_DB_SERVER)
+        cursor = conn.cursor()
+        get_values = {'uuid': uuid}
+        cursor.execute("SELECT m.data FROM metadata m WHERE m.uuid='%(uuid)s'") % get_values
+        record = cursor.fetchall()
+        if len(record) == 1:
+            return record
+        else:
+            raise ValueError('More than one metadata record was returned.  The metadataregistry table has duplicates!')
 
-	def getKeyWords(self,soup):
-		keywords = soup.find("gmd:md_keywords")
-		deskeywords = soup.find("gmd:descriptivekeywords")
+    def request_resource_action(self, service_name, op, **kwargs):
+        url = self.SGS_URL
+        url = url + service_name + '/' + op
+        r = {"serviceRequest": {
+            "serviceName": service_name,
+            "serviceOp": op,
+            "params": kwargs}
+        }
 
+        resp = requests.post(url, data={'payload': Serializer.encode(r)})
+        if resp.status_code == 200:
+            data = resp.json()
+            if 'GatewayError' in data['data']:
+                #error = GatewayError(data['data']['Message'])
+                #error.trace = data['data']['Trace']
+                #raise error
+                pass
+            if 'GatewayResponse' in data['data']:
+                return data['data']['GatewayResponse']
 
-    
-	def getGeoExtent(self,soup):
-		bound_list = ["westboundlongitude","eastboundlongitude","northboundlatitude","southboundlatitude"]
-		bbox = dict()  
-		geo_extent = soup.find("gmd:geographicelement")
-		for i in bound_list:
-			pos = geo_extent.find("gmd:"+i).text.replace("\n","")
-			bbox[i] = float(pos)
-		return bbox	
+    def getnameinfo(self, soup):
+        ab_info = soup.find("gmd:abstract")
+        pur_info = soup.find("gmd:purpose")
+        file_ident = soup.find("gmd:fileidentifier")
+        return file_ident.text.replace("\n", "")
 
-	def getTemporalExtent(self,soup):                       
-		temporal_extent = soup.find("gmd:temporalelement")
-		start_dt = temporal_extent.find("gml:beginposition").text.replace("\n","")
-		end_dt = temporal_extent.find("gml:endposition").text.replace("\n","")
-		return [start_dt,end_dt]    
+    def getidentinfo(self, soup):
+        indent_info = soup.find("gmd:identificationinfo")
+        title = indent_info.find("gmd:title").text.rstrip()
+        alt_title = indent_info.find("gmd:alternatetitle").text.replace("\n", "")
+        iden = indent_info.find("gmd:identifier").text.replace("\n", "")
+        org_name = indent_info.find("gmd:organisationname").text.replace("\n", "")
+        poc = indent_info.find("gmd:pointofcontact")
+        return title
+
+    def getkeywords(self, soup):
+        keywords = soup.find("gmd:md_keywords")
+        deskeywords = soup.find("gmd:descriptivekeywords")
+
+    def getgeoextent(self, soup):
+        bound_list = ["westboundlongitude", "eastboundlongitude", "northboundlatitude", "southboundlatitude"]
+        bbox = dict()
+        geo_extent = soup.find("gmd:geographicelement")
+        for i in bound_list:
+            pos = geo_extent.find("gmd:"+i).text.replace("\n","")
+            bbox[i] = float(pos)
+        return bbox
+
+    def get_temporal_extent(self, soup):
+        temporal_extent = soup.find("gmd:temporalelement")
+        start_dt = temporal_extent.find("gml:beginposition").text.replace("\n", "")
+        end_dt = temporal_extent.find("gml:endposition").text.replace("\n", "")
+        return [start_dt, end_dt]
 
 
 class Serializer:
+    """
+    Serializes JSON data
+    """
 
-	@classmethod
-	def encode(cls, message):
-		return json.dumps(message)
+    def __init__(self):
 
-	@classmethod
-	def decode(cls, message):
-		return json.loads(message, object_hook=cls._obj_hook)
+    @classmethod
+    def encode(cls, message):
+        return json.dumps(message)
 
-	@classmethod
-	def _obj_hook(cls, dct):
-		if '__np__' in dct:
-			dct = dct['__np__']
-			return np.array(dct['data'], dtype=dct['dtype'])
-		return dct
+    @classmethod
+    def decode(cls, message):
+        return json.loads(message, object_hook=cls._obj_hook)
+
+    @classmethod
+    def _obj_hook(cls, dct):
+        if '__np__' in dct:
+            dct = dct['__np__']
+            return np.array(dct['data'], dtype=dct['dtype'])
+        return dct

ca258a42a3541829b8efb810b774871c5860657c
Andy Bird <abird@asascience.com>
2014-06-26 13:00:27 -0400

    use SGS to get data product id used by erddap

--- fdw/covfdw/__init__.py
+++ fdw/covfdw/__init__.py
@@ -13,7 +13,7 @@ from multicorn import ForeignDataWrapper
 from multicorn import Qual
 from multicorn.utils import log_to_postgres,WARNING,ERROR
 from numpy.random import random
-import simplejson
+import simplejson as json
 import requests
 import random
 import os 
@@ -27,7 +27,8 @@ cov_fail_data_size = 100
 #used for by passing coverage issues and generating mock data
 debug = False
 
-SERVER = "http://erddap-test.oceanobservatories.org:8080/erddap/tabledap/"
+SERVER = "http://localhost:8005/erddap/tabledap/"
+SGS_URL = "http://localhost:5000/ion-service"
 
 class CovFdw(ForeignDataWrapper):
     '''
@@ -52,68 +53,151 @@ class CovFdw(ForeignDataWrapper):
         
 
     def execute(self, quals, req_columns):
-        #initiall set it to false
-        cov_available = False 
+        #initiall set it to false        
         param_list = []
         #data must really be in this format
         master_cols = self.columns.keys()
 
+        time_bounds = False
         for qual in quals:
                 if (qual.field_name == TIME):
+                    time_bounds = True
                     log_to_postgres(
                         "qualField:"+ str(qual.field_name) 
                         + " qualOperator:" + str(qual.operator) 
                         + " qualValue:" +str(qual.value), WARNING)
 
 
-        for param in master_cols:
-            if param == "lat":
-                param = "latitude"
-            elif param == "lon":
-                param = "longitude"
-            
-            param_list.append(param)    
+        self.logger.info("quals:"+str(quals))
 
-        self.logger.info("DataFields Requested:"+str(param_list)+"\n"+"master:"+ str(master_cols))
+        for param in master_cols:
+            #if param == "lat":
+            #    param = "latitude"
+            #elif param == "lon":
+            #    param = "longitude"
+            
+            param_list.append(param)           
 
         #check that the length requested is the same as the length available
-        if len(req_columns) == len(master_cols):
-            ret_data = self.getSimpleDataStruct(param_list)
+        if len(param_list) == len(master_cols):
+            ret_data = self.getSimpleDataStruct(param_list,time_bounds)
+            
+            #if there is data
             for row in ret_data:
                 yield row   
         else:
             #length is different               
             self.logger.info("DataFields Requested dont match those availabe")
+            self.logger.info("DataFields   Requested:"+str(param_list))
+            self.logger.info("masterFields Requested:"+ str(master_cols))                        
+            self.logger.info(str(len(param_list))+" vs " + str(len(master_cols)))
 
 
-    def getSimpleDataStruct(self,param_list):
+    def getSimpleDataStruct(self,param_list,time_bounds):
         '''
         used to obtain the data when all fields are requested
         i.e a simple/typical request 
         '''
         self.logger.info("resourceID:",self.cov_id)
-
+        cov_available = False 
         try:
-            self.logger.info("getting data from errdap")
-            time_bounds = "&time%3E=2014-02-22T21:51:42.615Z&time%3C=2014-02-22T22:11:46.501Z"
-            resource = "data"+"a990236fb3184d6bbefec7cc267ce307"
-            url = SERVER+resource+".json?"+ ",".join(param_list)+time_bounds
-            
-            if "time" in param_list:
-                url +="&orderBy(%22time%22)"
-                            
-            self.logger.info(url)
-            r = requests.get(url)
-            if r.status_code == 200:                
-                ret_data = r.json()                
-                ret_data = ret_data["table"]["rows"]
-                self.logger.info("got data...")
-                cov_available = True
-            else:    
-                self.logger.info("Could not get data..."+r.text)
-        except Exception, e:            
+
+            dataproduct_id,_ = self.request_resource_action('resource_registry', 'find_subjects', **{"object":self.cov_id, "predicate":'hasDataset', "id_only":True})
+            if (len(dataproduct_id))> 0:
+                dp_id = dataproduct_id[0]
+                self.logger.info("data product id from sgs:"+str((dataproduct_id[0])))
+                
+                #create time bounds if available
+                if time_bounds:
+                    time_bounds_str = "&time%3E=2014-02-22T21:51:42.615Z&time%3C=2014-02-22T22:11:46.501Z"
+                else:
+                    time_bounds_str = ""
+
+                resource = "data" + dp_id
+                #create url
+                url = SERVER+resource+".json?"+ ",".join(param_list)+time_bounds_str
+                
+                #if time is in there add the orderby
+                if "time" in param_list:
+                    url +="&orderBy(%22time%22)"
+                                
+                self.logger.info(url)
+                r = requests.get(url)
+                if r.status_code == 200:
+                    #if available                
+                    ret_data = r.json()                
+                    ret_data = ret_data["table"]["rows"]
+                    self.logger.info("got data...")
+                    cov_available = True
+                elif "Your query produced no matching results" in r.text:
+                    #no data
+                    self.logger.info("not data for request...")
+                    return None
+                else:    
+                    #error
+                    self.logger.info("Could not get data..."+r.text)
+                    return None
+        except Exception, e:
+                #fail            
                 self.logger.error("Failed to get data...:" + str(e))            
+
         
         #loads a coverage
         if cov_available:
-            return ret_data           
+            return ret_data 
+        else:
+            return None       
+
+
+
+
+
+    def request_resource_action(self, service_name, op, **kwargs):  
+
+        url = SGS_URL
+        url = "/".join([url, service_name, op])
+        #self.logger.info("url:"+url)
+             
+        r = {"serviceRequest": {
+            "serviceName": service_name,
+            "serviceOp": op,
+            "params": kwargs}
+        }
+
+        resp = requests.post(url, data={'payload': Serializer.encode(r)})        
+
+        if "<h1>Not Found</h1>" in resp.text:
+             self.logger.info("service gateway service not found")     
+        else:
+            if resp.status_code == 200:
+                data = resp.json()
+                if 'GatewayError' in data['data']:
+                    error = GatewayError(data['data']['Message'])
+                    self.logger.info("GATEWAY ERROR:"+str(error))     
+                if 'GatewayResponse' in data['data']:
+                    return data['data']['GatewayResponse']   
+                    
+
+
+class Serializer:
+    """
+    Serializes JSON data
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def encode(cls, message):
+        return json.dumps(message)
+
+    @classmethod
+    def decode(cls, message):
+        return json.loads(message, object_hook=cls._obj_hook)
+
+    @classmethod
+    def _obj_hook(cls, dct):
+        if '__np__' in dct:
+            dct = dct['__np__']
+            return np.array(dct['data'], dtype=dct['dtype'])
+        return dct                                
